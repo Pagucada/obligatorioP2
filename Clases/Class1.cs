b7466d0406e5f58456b7d0c67a08c6d727264b89
PotatoGrrr <lucasahre05@gmail.com>
2022-09-20 22:46:27 -0300

    2

--- Clases/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Clases
-{
-    public class Class1
-    {
-        public string nombre { get; set; }
-    }
-}
--- obligatorioCodFuente/Program.cs
+++ obligatorioCodFuente/Program.cs
@@ -7,10 +7,7 @@ namespace obligatorioCodFuente
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cambio 1");
-            Console.WriteLine("holis");
-            Class1 hola = new Class1();
-            hola.nombre = "sdakhj";
+            
         }
     }
 }
