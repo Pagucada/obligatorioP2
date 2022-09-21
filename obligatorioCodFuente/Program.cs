47d57b8f5a828b295219922c5fd12f2222298930
PotatoGrrr <lucasahre05@gmail.com>
2022-09-20 22:34:12 -0300

    1

---
+++ obligatorioCodFuente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace obligatorioCodFuente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
