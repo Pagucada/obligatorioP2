e224c3fe11d6dccc16d4a20b4cc800f9b644458d
PotatoGrrr <lucasahre05@gmail.com>
2022-09-20 22:36:38 -0300

    Update Program.cs

--- obligatorioCodFuente/Program.cs
+++ obligatorioCodFuente/Program.cs
@@ -6,7 +6,7 @@ namespace obligatorioCodFuente
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cambio 1");
         }
     }
 }
