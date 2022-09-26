ca61f87d19b640db72c1071180e26f962266e5ae
PotatoGrrr <lucasahre05@gmail.com>
2022-09-25 23:43:32 -0300

    a

---
+++ Clases/Sistema.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases
+{
+    class Sistema
+    {
+        private static Sistema Instancia = null;
+        private List<Jugador> Jugadores = new List<Jugador>();
+        private List<Seleccion> Selecciones = new List<Seleccion>();
+        private List<Pais> Paises = new List<Pais>();
+        private List<Partido> Partidos = new List<Partido>();
+        private List<Resenia> Resenia = new List<Resenia>();
+
+
+        private Sistema()
+        {
+            //precarga
+        }
+
+        public Sistema getInstancia() { 
+        if(Instancia == null)
+            {
+                Instancia = new Sistema();
+            }
+            return Instancia;
+        }
+
+    }
+}
