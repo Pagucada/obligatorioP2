6fe98607572fd2a7ac78d5c17b5ebb33adc9f88c
PotatoGrrr <lucasahre05@gmail.com>
2022-09-26 19:54:25 -0300

    aa

--- Clases/Partido.cs
+++ Clases/Partido.cs
@@ -6,11 +6,18 @@ namespace Clases
 {
     class Partido : IValidacion
     {
-        
-        
-        
-        
-        
+
+        public int Id { get; }
+        public static int UltimoId { get; } = 1;
+        public Seleccion Seleccion1 { get; set; }
+        public Seleccion Seleccion2 { get; set; }
+        public DateTime FechaYHora { get; set; }
+        public bool Finalizado { get; set; }
+        public List<Incidencia> Incidencias { get; set; } = new List<Incidencia>();
+        public string ResultadoFInal { get; set; }
+
+
+
         public void ValidarDatos()
         {
             throw new NotImplementedException();
